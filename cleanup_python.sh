9d37de03f382feea2d52c69dd5ab36901f87cb11
lyh01 <huy_ly@hotmail.com>
2020-12-01 19:42:47 -0600

    Create cleanup_python.sh

---
+++ cleanup_python.sh
@@ -0,0 +1,10 @@
+apt remove libpython2.7-minimal libpython2.7-stdlib libpython2.7 -y
+apt remove libpython3.5-minimal libpython3.5-stdlib libpython3.5 -y
+apt autoremove -y
+rm -r /opt/python/2.7.17
+rm -r /opt/python/2.7
+rm -r /opt/python/2
+rm -r /opt/python/3.6.10
+rm -r /opt/python/3.6
+rm -r /opt/python/3.7.7
+rm -r /opt/python/3.7
